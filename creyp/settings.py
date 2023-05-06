dced798e2b7ea7f93d5fce6b6ab177b6eb8ffe6a
Divuzki <divuzki@gmail.com>
2023-05-06 15:48:41 +0100

    Updated `requirement.txt`

--- creyp/settings.py
+++ creyp/settings.py
@@ -116,8 +116,8 @@ DATABASES = {
     }
 }
 
-# db_from_env = dj_database_url.config(conn_max_age=600)
-# DATABASES['default'].update(db_from_env)
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
