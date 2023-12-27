18e1c42a271843115f1b6d5a30986ec041c32765
Divine Ikhuoria <divuzki@gmail.com>
2023-12-27 10:26:21 +0100

    Add JivoSite chat widget and update user contact info

--- users/models.py
+++ users/models.py
@@ -30,8 +30,6 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(blank=True, upload_to="profile-image/%h/%Y/",
                               default="profile-image-placeholder.png")
-    first_name = models.CharField(max_length=100, blank=True)
-    last_name = models.CharField(max_length=100, blank=True)
     email = models.EmailField(max_length=150, blank=True)
     gender = models.CharField(max_length=15, choices=GENDER, blank=True)
     phone_number = models.CharField(max_length=15, blank=True)
@@ -45,6 +43,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+    
+    @property
+    def first_name(self):
+        return self.user.first_name
+    
+    @property
+    def last_name(self):
+        return self.user.last_name
 
     def save(self, *args, **kwargs):
         if self.image:
